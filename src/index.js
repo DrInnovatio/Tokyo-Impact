57a0e4c8d3f7f89abb27da3789a1e22790509829
DrInnovatio <turing1723@gmail.com>
2020-10-21 19:18:59 +1000

    added react-router-dom.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  ,document.getElementById('root')
 );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
